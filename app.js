1308a1de07cb6870c6aab1a5277819e74e140d56
motokitakumi <mtktkm242424@gmail.com>
2020-12-27 09:52:03 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const question = 'この中でいちばん身長が大きいのは？';
+const answers = [
+    'ウルトラマン',
+    'サザエさん',
+    'ゴジラ',
+    'ガンダム'
+];
+const correct = 'ゴジラ';
+
+document.getElementById('js-question').textContent = question;
+
+const $button = document.getElementsByTagName('button')
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
+
+$button[0].addEventListener('click', () => {
+    if(correct === $button[0].textContent){
+        window.alert('正解!');
+    } else {
+        window.alert('不正解!');
+    }
+});
+
+$button[1].addEventListener('click', () => {
+    if(correct === $button[1].textContent){
+        window.alert('正解!');
+    } else {
+        window.alert('不正解!');
+    }
+});
+
+$button[2].addEventListener('click', () => {
+    if(correct === $button[2].textContent){
+        window.alert('正解!');
+    } else {
+        window.alert('不正解!');
+    }
+});
+
+$button[3].addEventListener('click', () => {
+    if(correct === $button[3].textContent){
+        window.alert('正解!');
+    } else {
+        window.alert('不正解!');
+    }
+});
